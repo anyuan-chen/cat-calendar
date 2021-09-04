80d791074ae1f8c3237577eff07902aaf53d5e4b
classidied <claire.wang028@gmail.com>
2021-09-04 18:27:36 -0400

    buttons or something

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Habit.css";
 
 export default function Habit(props) {
   return (
+      
     <div>
-      <tr>
+        <button type="button">
+            <img src={props.img} />
+            <p>{props.name}</p>
+        </button>
+    {/*
+    the thing that was here before I experiment
+    <tr>
         <td>{props.name}</td>
         <td>{props.img}</td>
-      </tr>
+    </tr>
+    */}
+
     </div>
   );
 }
--- src/pages/Habits.js
+++ src/pages/Habits.js
@@ -6,15 +6,22 @@ import Footer from "../components/Footer";
 
 export default function Habits() {
     return (
-        <div class="main">
+        <div>
             <Navbar></Navbar>
-            <h1>Habit Builder</h1>
-            <h2 class="header">Select a few of our pre-created habits to add to your schedule!</h2>
-            <h2>Each habit will take up 15 minutes.</h2>
-            <div class="selection">
-                <Habit></Habit>
-            </div>
-            <Footer></Footer>
+            <body>
+                <h1>Habit Builder</h1>
+                <h2 class="header">Select a few of our pre-created habits to add to your schedule!</h2>
+                <h2>Each habit will take up 15 minutes.</h2>
+                <div class="selection">
+                    <Habit></Habit>
+                    <Habit></Habit>
+                    <Habit></Habit>
+                    <Habit></Habit>
+                </div>
+                {/*<button type="submit">Next</button> */}
+                <Footer></Footer>
+            </body>
+            
         </div>
     )
 }
