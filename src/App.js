ced383a1d0ad4b26582e5ecf106a3cc886b1c5a7
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 14:06:06 -0400

    starter project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar"
 function App() {
-  return <div></div>;
+  return (
+    <Navbar></Navbar>
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/"></Route>
+    //     <Route exact path="/stats"></Route>
+    //     <Route exact path="/dashboard"></Route>
+    //   </Switch>
+    // </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Navbar.css";
+export default function Navbar() {
+  return (
+    <div>
+      <h1>hellow</h1>
+    </div>
+  );
+}
