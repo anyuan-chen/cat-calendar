a5afa9755b5f2cf0a451159c521888e8136fc7f9
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 15:52:00 -0400

    footer

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function Task() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function Task(props) {
+  return (
+    <div>
+      <tr>
+        <td>{props.name}</td>
+        <td>{props.start}</td>
+        <td>{props.end}</td>
+      </tr>
+    </div>
+  );
 }
