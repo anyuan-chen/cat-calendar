6fc4bf9d83ceb81dfe823cc58e7c22c72cc340f3
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 15:03:15 -0400

    delete sample code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,7 @@ import Navbar from "../components/Navbar";
 export default function Home(props) {
   return (
     <div>
-      <h1>{props.hello}</h1>
       <Navbar></Navbar>
-      <img src={process.env.PUBLIC_URL + "/img/sample.jpeg"} />;
       <img src={process.env.PUBLIC_URL + "/img/homepage.jpeg"} />;
     </div>
   );
