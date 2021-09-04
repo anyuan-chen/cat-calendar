f4ab1ec82116998bcb27370050d4470bd71a9757
classidied <claire.wang028@gmail.com>
2021-09-04 15:28:10 -0400

    css??

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
         <div>
           <img src={process.env.PUBLIC_URL + "/img/Header cat.png"} alt="uh oh"/>
         </div>
-        <div>
+        <div class="navigation">
           <Link to="/home">
             <h2>Home</h2>
           </Link>
