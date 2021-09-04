46fbd11114edfc37670bc29b7ecec136e72cf675
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 16:39:04 -0400

    improved navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,26 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 export default function Navbar() {
   return (
-    <div>
-      <nav>
-        <div class="main">
-          <a
-            href="/home"
+    <nav className="navbar">
+      <div class="main">
+        <a href="/home">
+          <img
             src={process.env.PUBLIC_URL + "/img/Header cat.png"}
             alt="uh oh"
           />
-          <a />
-        </div>
-        <div class="navigation">
-          <Link to="/">
-            <h2>Home</h2>
-          </Link>
-          <Link to="/events">
-            <h2>Events</h2>
-          </Link>
-          <Link to="/habits">
-            <h2>Habits</h2>
-          </Link>
-        </div>
-      </nav>
-    </div>
+        </a>
+      </div>
+      <div class="navigation">
+        <Link to="/" className="navlink">
+          <h2>Home</h2>
+        </Link>
+        <Link to="/events" className="navlink">
+          <h2>Events</h2>
+        </Link>
+        <Link to="/habits" className="navlink">
+          <h2>Habits</h2>
+        </Link>
+      </div>
+    </nav>
   );
 }
