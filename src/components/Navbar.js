58d63aaef8953c87f900a89b594c79db35f8c012
classidied <claire.wang028@gmail.com>
2021-09-04 16:05:06 -0400

    andrew help

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,17 +10,16 @@ export default function Navbar() {
             src={process.env.PUBLIC_URL + "/img/Header cat.png"}
             alt="uh oh"
           />
-          ;
         </div>
         <div class="navigation">
           <Link to="/home">
             <h2>Home</h2>
           </Link>
-          <Link to="/habits">
-            <h2>Habits</h2>
-          </Link>
           <Link to="/events">
             <h2>Events</h2>
+          </Link>
+          <Link to="/habits">
+            <h2>Habits</h2>
           </Link>
         </div>
       </nav>
