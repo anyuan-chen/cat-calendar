dde8e8b64acc37674170f7323cb89f4eb360cfb2
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 17:24:25 -0400

    footer

--- src/pages/Habits.js
+++ src/pages/Habits.js
@@ -14,6 +14,7 @@ export default function Habits() {
             <div class="selection">
                 <Habit></Habit>
             </div>
+            <Footer></Footer>
         </div>
     )
 }
