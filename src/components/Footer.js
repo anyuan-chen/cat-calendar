18d2e6d32873d61acea88bead7754e5cd04c84d9
RammyMcRamster <82988557+RammyMcRamster@users.noreply.github.com>
2021-09-04 15:57:14 -0400

    a

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div>
-     <h1>Copyright ©2021 Cat Calendars Co.</h1>
+     <h1 class="copyright" >Copyright ©2021 Cat Calendars Co.</h1>
     </div>
   );
 }
