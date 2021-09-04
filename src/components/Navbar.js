e7a9a82f3650136aaee59844fac0a360ef04f517
RammyMcRamster <82988557+RammyMcRamster@users.noreply.github.com>
2021-09-04 17:00:25 -0400

    good lord

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,26 +10,6 @@ export default function Navbar() {
             src={process.env.PUBLIC_URL + "/img/Header cat.png"}
             alt="uh oh"
           />
-<<<<<<< HEAD
-          <a />
-        </div>
-        <div class="navigation">
-          <Link to="/">
-            <h2>Home</h2>
-          </Link>
-          <Link to="/events">
-            <h2>Events</h2>
-          </Link>
-          <Link to="/habits">
-            <h2>Habits</h2>
-          </Link>
-          <Link to="/schedule">
-            <h2>Schedule</h2>
-          </Link>
-        </div>
-      </nav>
-    </div>
-=======
         </a>
       </div>
       <div class="navigation">
@@ -44,6 +24,5 @@ export default function Navbar() {
         </Link>
       </div>
     </nav>
->>>>>>> efb38dc252dea187cdee21848fa18cd5a86912c6
   );
 }
