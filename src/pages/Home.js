7e9f053d249e698fa88cd3911b23afdbf8e8af95
RammyMcRamster <82988557+RammyMcRamster@users.noreply.github.com>
2021-09-04 15:44:21 -0400

    oho

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home(props) {
   return (
