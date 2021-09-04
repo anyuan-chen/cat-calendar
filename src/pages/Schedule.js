7584844cb946af146219e1ec65e9471f85fd6b60
RammyMcRamster <82988557+RammyMcRamster@users.noreply.github.com>
2021-09-04 17:14:45 -0400

    i am dead inside

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import "./Schedule.css";
 
 export default function Schedule() {
-  return <div>Schedule</div>;
+  return (
+  <div>Schedule</div>
+
+
+
+
+  );
 }
