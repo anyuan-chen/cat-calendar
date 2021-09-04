65f1c30e0f27e74b213265fb155927938250a663
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 18:17:19 -0400

    events.js logic for event items

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import "./Events.css";
 import Footer from "../components/Footer";
+import Task from "../components/Task";
 
 export default function Events() {
+  const addToLocalStorage = (event) => {
+    event.preventDefault();
+    if (!localStorage.tasks) {
+      localStorage.setItem(
+        "tasks",
+        JSON.stringify([
+          {
+            task: task,
+            startTime: startTime,
+            endTime: endTime,
+          },
+        ])
+      );
+    } else {
+      let storedTasks = JSON.parse(localStorage.tasks);
+      storedTasks.push({
+        task: task,
+        startTime: startTime,
+        endTime: endTime,
+      });
+      localStorage.setItem("tasks", JSON.stringify(storedTasks));
+    }
+  };
   const [task, setTask] = useState("");
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
-
+  const [prevTasks, setPrevTasks] = useState([]);
+  useEffect(() => {
+    if (localStorage.tasks) {
+      setPrevTasks(JSON.parse(localStorage.tasks));
+    }
+  }, [localStorage]);
   return (
-    <form onSubmit={() => {}}>
+    <form onSubmit={addToLocalStorage}>
       <Navbar></Navbar>
       <div className="container">
         <h1>Event Builder</h1>
@@ -48,6 +77,13 @@ export default function Events() {
                 ></input>
               </th>
             </tr>
+            {prevTasks.map((task) => {
+              <Task
+                name={task.task}
+                start={task.startTime}
+                end={task.endTime}
+              ></Task>;
+            })}
           </tbody>
         </table>
         <button type="submit">Add</button>
