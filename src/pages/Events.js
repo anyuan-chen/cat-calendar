43c267b676063c3851f69e6f1882060c2598c12e
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 17:21:33 -0400

    event

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -3,30 +3,30 @@ import Navbar from "../components/Navbar";
 import "./Events.css";
 export default function Events() {
   return (
-    <div>
+    <form>
       <Navbar></Navbar>
       <div className="container">
         <h1>Event Builder</h1>
-        <table>
-          <colgroup>
-            <col class="w"></col>
-          </colgroup>
-          <thead>
-            <tr>
-              <th>Task</th>
-              <th>Start Time</th>
-              <th>End Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>hi</th>
-              <th>hi</th>
-              <th>hi</th>
-            </tr>
-          </tbody>
-        </table>
+          <table>
+            <colgroup>
+              <col class="w"></col>
+            </colgroup>
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th><input type="text"></input></th>
+                <th>hi</th>
+                <th>hi</th>
+              </tr>
+            </tbody>
+          </table>
       </div>
-    </div>
+    </form>
   );
 }
