ee4e7ddddaeea0b0b9a40b9a56738c24af060477
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 18:00:14 -0400

    kekw

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 import "./Events.css";
 import Footer from "../components/Footer";
 
-
 export default function Events() {
   const [task, setTask] = useState("");
   const [startTime, setStartTime] = useState("");
