32e82343d82d3fbcdc54498bec133b25808123c3
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 22:22:47 -0400

    i handle errors like i handle social situations

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,31 +1,44 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import "./Schedule.css";
 import Footer from "../components/Footer";
-import {algorithm} from "../util/algorithm"
+import algorithm from "../util/algorithm";
 
 export default function Schedule() {
   useEffect(() => {
-    const tasks = JSON.parse(localStorage.tasks);
-    const habits = JSON.parse(localStorage.habits);
-    const scheduleArray = algorithm(habits, tasks);
-    
-  }, [])
+    let tasks;
+    if (localStorage.tasks) {
+      tasks = JSON.parse(localStorage.tasks);
+    }
+    let habits;
+    if (localStorage.habits) {
+      habits = JSON.parse(localStorage.habits);
+    }
+    let scheduleArray;
+    if (tasks && habits) {
+      scheduleArray = algorithm(habits, tasks);
+    }
+  }, []);
+
   return (
     <div>
-        <Navbar></Navbar>
-        <div class="iNeedAChiropractorSoBad">
-        <img src="/img/background.png" alt="background"/>
+      <Navbar></Navbar>
+      <div class="iNeedAChiropractorSoBad">
+        <img src="/img/background.png" alt="background" />
         <h1 id="yourSchedule">Your Schedule</h1>
+        <div>
+          <img
+            id="path"
+            src="/img/rectanglePath.png"
+            alt="pink rectangle path"
+          />
           <div>
-            <img id="path" src="/img/rectanglePath.png" alt="pink rectangle path"/>
-            <div>
-              <img id="dottedLine" src="/img/dotted line.png" alt="dotted line"/>
-            </div>
+            <img id="dottedLine" src="/img/dotted line.png" alt="dotted line" />
           </div>
         </div>
-        
-        <Footer></Footer>
+      </div>
+
+      <Footer></Footer>
     </div>
   );
 }
