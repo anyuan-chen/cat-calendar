e40ca587e70673f0e866516bb07164986bf9e453
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-04 23:07:12 -0400

    algo problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Schedule from "./pages/Schedule";
 import Habits from "./pages/Habits";
