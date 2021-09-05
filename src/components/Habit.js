b1207dc1be00f4d5f041fcda8c7820c92292075a
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-05 00:44:08 -0400

    WE ARE PROGRAMMERS

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Habit.css";
 
 export default function Habit(props) {
+  const [selected, setSelected] = useState(false);
+
   return (
-      
     <div class="container">
-        <button type="button" id="habit">
-            <img class="icon" src={process.env.PUBLIC_URL + props.img} alt="missing"/> 
-        </button>
-        <p id="buttLabel">{props.name}</p>
+      <button
+        type="button"
+        id="habit"
+        className={selected ? "clicked" : "notClicked"}
+        onClick={() => setSelected(!selected)}
+      >
+        <img
+          class="icon"
+          src={process.env.PUBLIC_URL + props.img}
+          alt="missing"
+        />
+      </button>
+      <p id="buttLabel">{props.name}</p>
     </div>
   );
 }
