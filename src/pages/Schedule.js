ee354a3265cc7bcea4a3cb1c5497562251ae7c9b
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-05 00:33:53 -0400

    I LOVE SUGAR SUGAR IS FUN

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import "./Schedule.css";
 import Footer from "../components/Footer";
 import algorithm from "../util/algorithm";
 
 export default function Schedule() {
-  const [schedule, setSchedule] 
+  const [schedule, setSchedule] = useState([]);
   useEffect(() => {
     let tasks;
     if (localStorage.tasks) {
@@ -19,7 +19,6 @@ export default function Schedule() {
     if (tasks && habits) {
       scheduleArray = algorithm(habits, tasks);
     }
-
   }, []);
 
   return (
@@ -38,19 +37,45 @@ export default function Schedule() {
             <img id="dottedLine" src="/img/dotted line.png" alt="dotted line" />
           </div>
           <div id="times">
-            <label input type="text">8AM</label>
-            <label input type="text">9AM</label>
-            <label input type="text">10AM</label>
-            <label input type="text">11AM</label>
-            <label input type="text">12PM</label>
-            <label input type="text">1PM</label>
-            <label input type="text">2PM</label>
-            <label input type="text">3PM</label>
-            <label input type="text">4PM</label>
-            <label input type="text">5PM</label>
-            <label input type="text">6PM</label>
-            <label input type="text">7PM</label>
-            <label input type="text">8PM</label>
+            <label input type="text">
+              8AM
+            </label>
+            <label input type="text">
+              9AM
+            </label>
+            <label input type="text">
+              10AM
+            </label>
+            <label input type="text">
+              11AM
+            </label>
+            <label input type="text">
+              12PM
+            </label>
+            <label input type="text">
+              1PM
+            </label>
+            <label input type="text">
+              2PM
+            </label>
+            <label input type="text">
+              3PM
+            </label>
+            <label input type="text">
+              4PM
+            </label>
+            <label input type="text">
+              5PM
+            </label>
+            <label input type="text">
+              6PM
+            </label>
+            <label input type="text">
+              7PM
+            </label>
+            <label input type="text">
+              8PM
+            </label>
           </div>
         </div>
       </div>
