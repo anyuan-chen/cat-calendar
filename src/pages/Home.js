a2cb18bbbf51ecc06a9d529279586bd4a3ec9523
RammyMcRamster <82988557+RammyMcRamster@users.noreply.github.com>
2021-09-04 22:42:55 -0400

    sdfiubgdfsiu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,9 @@ import Footer from "../components/Footer";
 
 export default function Home(props) {
   return (
-    <div>
+    <div id="um">
       <Navbar></Navbar>
-      <img id="kitty" src={process.env.PUBLIC_URL + "/img/homepage.jpeg"} alt=""/>
+        <img id="kitty" src={process.env.PUBLIC_URL + "/img/homepage.jpeg"} alt=""/>
       <Footer></Footer>
     </div>
   );
