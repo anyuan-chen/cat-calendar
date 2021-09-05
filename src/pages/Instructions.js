139ab10ba65ac4a762d1fdba59da4b6bf6a73f09
RammyMcRamster <82988557+RammyMcRamster@users.noreply.github.com>
2021-09-05 19:48:57 -0400

    oh.

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import "./Instructions.css";
 import Footer from "../components/Footer";
 
 export default function Instructions() {
